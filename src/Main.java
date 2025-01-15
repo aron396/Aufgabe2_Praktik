ced6fb264d1098da6e243996b97558e34758f0d8
aron396 <bandi.aron2003@gmail.com>
2025-01-15 16:15:55 +0200

    Consolle-View

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+import static controller.Krankenhausmanagement.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true) {
+            showMenu();  // Zeigt das Hauptmenü der Anwendung an
+            int choice = scanner.nextInt();  // Benutzereingabe zur Auswahl einer Aktion
+            scanner.nextLine();  // Neue Zeile konsumieren, um Fehler zu vermeiden
+            switch (choice) {
+                case 1: addMedikament(); break;
+                case 2: displayMedikament(); break;
+                case 3: editMedikament(); break;
+                case 4: deleteMedikament(); break;
+                case 5: addPatient(); break;
+                case 6: displayPatients(); break;  // Alle Charaktere anzeigen
+                case 7: editPatient(); break;  // Charakter bearbeiten
+                case 8: deletePatient(); break;  // Charakter löschen
+                case 9: filterPatientenByDiagnose(); break;  // Charaktere nach Herkunftsort filtern
+                case 10: displayPatientenByMedikamentKrankheit(); break;  // Charaktere nach Produkt-Herkunftsregion filtern
+                case 11: sortPatientMedikamenteByPreis(); break;  // Produkte eines Charakters nach Preis sortieren
+                case 12: System.exit(0); break;  // Anwendung beenden
+                default: System.out.println("Ungültige Auswahl."); break;  // Ungültige Eingabe
+            }
         }
     }
 }
