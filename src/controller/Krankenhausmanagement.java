ca0de108047712d35b3db682ff883cc3179afafb
aron396 <bandi.aron2003@gmail.com>
2025-01-15 15:39:22 +0200

    Krankenhaus Controller hinzugefugt

---
+++ src/controller/Krankenhausmanagement.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class Krankenhausmanagement {
+
+    private static void showMenu() {
+        System.out.println("Wählen Sie eine Option:");
+        System.out.println("1. Medikamente hinzufügen");
+        System.out.println("2. Alle Medikamente anzeigen");
+        System.out.println("3. Medikamente bearbeiten");
+        System.out.println("4. Medikamente löschen");
+        System.out.println("5. Patiente hinzufügen");
+        System.out.println("6. Alle Patiente anzeigen");
+        System.out.println("7. Patiente bearbeiten");
+        System.out.println("8. Patiente löschen");
+        System.out.println("9. Patiente nach Diagnose filtern");
+        System.out.println("10. Patiente nach Medikament-Krankheit filtern");
+        System.out.println("11. Medikamente eines Patientes nach Preis sortieren");
+        System.out.println("12. Beenden");
+    }
+
+
+}
