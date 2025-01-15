17cc820dcf3825a885447a3e044675980d3810ea
aron396 <bandi.aron2003@gmail.com>
2025-01-15 16:20:47 +0200

    ohne Kommentare

--- src/Main.java
+++ src/Main.java
@@ -14,14 +14,14 @@ public class Main {
                 case 3: editMedikament(); break;
                 case 4: deleteMedikament(); break;
                 case 5: addPatient(); break;
-                case 6: displayPatients(); break;  // Alle Charaktere anzeigen
-                case 7: editPatient(); break;  // Charakter bearbeiten
-                case 8: deletePatient(); break;  // Charakter löschen
-                case 9: filterPatientenByDiagnose(); break;  // Charaktere nach Herkunftsort filtern
-                case 10: displayPatientenByMedikamentKrankheit(); break;  // Charaktere nach Produkt-Herkunftsregion filtern
-                case 11: sortPatientMedikamenteByPreis(); break;  // Produkte eines Charakters nach Preis sortieren
-                case 12: System.exit(0); break;  // Anwendung beenden
-                default: System.out.println("Ungültige Auswahl."); break;  // Ungültige Eingabe
+                case 6: displayPatients(); break;
+                case 7: editPatient(); break;
+                case 8: deletePatient(); break;
+                case 9: filterPatientenByDiagnose(); break;
+                case 10: displayPatientenByMedikamentKrankheit(); break;
+                case 11: sortPatientMedikamenteByPreis(); break;
+                case 12: System.exit(0); break;
+                default: System.out.println("Ungültige Auswahl."); break;
             }
         }
     }
